0c15e6a2908276a2a4cb3a7088f7c4968fcd1dc6
James Kong <k.kong@actimirror.com>
2016-04-15 14:42:53 +0800

    experiment kmean

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "ofMain.h"
 #include "ofApp.h"
-
 //========================================================================
 int main( ){
 	ofSetupOpenGL(1024,1024,OF_WINDOW);			// <-------- setup the GL context
